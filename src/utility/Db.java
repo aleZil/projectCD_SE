146a22b7850ecfc2722fda9b90cb219d0d384cc9
Alessandro <alexzil93@gmail.com>
2017-06-20 15:30:51 +0200

    update db class

--- src/utility/Db.java
+++ src/utility/Db.java
@@ -1,9 +1,24 @@
 package utility;
 
+import java.sql.*;
+
 public class Db {
 
-	
-	
-	
-	
+	static Connection getConnection() throws Exception {
+		
+        String dbName = "progetto_cd";
+        String userName = "hanzo";
+        String password = "neversurrender";
+        
+        try{
+        	
+        	Connection con=DriverManager.getConnection("jdbc:postgresql://db-cdproject.czz77hrlmvcn.eu-west-1.rds.amazonaws.com/" + dbName, userName, password);
+            return con;
+            
+        } catch (SQLException e) {
+        	
+        	// TODO da sistemare sta cosa
+        	return null;
+        }
+	}
 }
