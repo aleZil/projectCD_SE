6a20ee1cbdfd13bc902626a1f51edfe78d90c157
Alessandro <alexzil93@gmail.com>
2017-06-20 17:27:47 +0200

    aggiunto model inizio

---
+++ src/model/Model.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.sql.*;
+
+import utility.*;
+
+
+public class Model {
+
+	
+	Connection con = null;
+	
+	
+	
+	
+	
+	
+	
+	/*
+	try {
+		Connection con = Db.getConnection();
+	
+	} catch (SQLException e) {
+		System.out.println("Errore durante la connessione");
+	}
+    */
+}
