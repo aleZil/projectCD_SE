5853ab7ee0422b07c01c6ecf05534900143dadbd
Andrea Trevisan <Andrea@MacBook-Pro-di-Andrea.local>
2017-07-06 18:49:03 +0200

    payload

--- view/viewAreaRiservata/aggiungiStrumentoWnd.java
+++ view/viewAreaRiservata/aggiungiStrumentoWnd.java
@@ -62,11 +62,11 @@ public class aggiungiStrumentoWnd extends JFrame{
 	private JFrame caller;
 	private JList<String> list;
 	private DefaultListModel<String> listModel3;
-	
+
 	Map<String,Integer> kStrumenti;
 	private JComboBox<String> cbStrumenti;
-	
-	
+
+
 	public aggiungiStrumentoWnd(JFrame caller) {
 		//Tengo il riferimento al main form
 		this.caller=caller;
@@ -75,71 +75,71 @@ public class aggiungiStrumentoWnd extends JFrame{
 		this.addWindowListener(new closerAddInstrumentListener(this));
 		loadModel();		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	//chiude il frame
-		setBounds(caller.getLocation().x,caller.getLocation().y, 700, 170);
+		setBounds(caller.getLocation().x,caller.getLocation().y, 750, 170);
 		this.setLocation(caller.getLocation().x+200, caller.getLocation().y+200);
 		getContentPane().setLayout(new MigLayout("", "[100.00][200.00,grow][230.00][100.00]", "[grow,top][grow,top][grow,top][grow,top]"));
-		
+
 		JLabel lblSelezionaStrumento = new JLabel("Seleziona strumento:");
 		lblSelezionaStrumento.setFont(new Font("Dialog", Font.BOLD, 14));
 		getContentPane().add(lblSelezionaStrumento, "cell 0 0,alignx trailing,aligny center");
-		
+
 		JScrollPane listPanel = new JScrollPane();
-		getContentPane().add(listPanel, "cell 2 0 1 4,grow");
+		getContentPane().add(listPanel, "cell 2 0 1 3,grow");
 		list = new JList(listModel3);
 		list.setFont(new Font("Dialog", Font.BOLD, 14));
 		listPanel.setViewportView(list);
-		
-		JButton btnAggiungiStrumento = new JButton("Aggiungi strumento");
-		btnAggiungiStrumento.setFont(new Font("Dialog", Font.BOLD, 14));
-		getContentPane().add(btnAggiungiStrumento, "cell 1 2,alignx center,aligny center");
-		btnAggiungiStrumento.addActionListener(new btnAddInstrumentListener(this));
 
-		JButton btnRimuoviStrumento = new JButton("Rimuovi strumento");
-		btnRimuoviStrumento.setFont(new Font("Dialog", Font.BOLD, 14));
-		getContentPane().add(btnRimuoviStrumento, "cell 3 2,alignx center,aligny center");
-		btnRimuoviStrumento.addActionListener(new btnRemoveInstrumentListener(this));
-		
 		//ComboBox
 		cbStrumenti = new JComboBox();
 		cbStrumenti.setFont(new Font("Dialog", Font.PLAIN, 14));
-		
+
 		//da qui in poi per mostrare la lista di tutti gli strumenti nella ComboBox
 		ArrayList<Strumento> listaStrumenti = new Strumento().getAll();
-		
+
 		//TODO
 		//Se l'utente aveva scritto prima,pulisco
 		//clearComponents();			
-		
+
 		kStrumenti = new HashMap<String,Integer>();
 
 		//Rimuovo gli elementi che eventualmente ci sono
 		cbStrumenti.removeAll();	
-						
+
 		for (int i=0; i < listaStrumenti.size(); i++) {
 			Strumento strumento = listaStrumenti.get(i);
 			kStrumenti.put(strumento.getNome(), strumento.getId());
 			cbStrumenti.addItem(strumento.getNome());
 		}
-				
+
 		getContentPane().add(cbStrumenti, "cell 1 0,growx,aligny center");
-		
+
+		JButton btnAggiungiStrumento = new JButton("Aggiungi strumento");
+		btnAggiungiStrumento.setFont(new Font("Dialog", Font.BOLD, 14));
+		getContentPane().add(btnAggiungiStrumento, "cell 1 3,alignx center,aligny center");
+
+		JButton btnRimuoviStrumento = new JButton("Rimuovi strumento");
+		btnRimuoviStrumento.setFont(new Font("Dialog", Font.BOLD, 14));
+		getContentPane().add(btnRimuoviStrumento, "cell 2 3,alignx center,aligny center");
+		btnRimuoviStrumento.addActionListener(new btnRemoveInstrumentListener(this));
+		btnAggiungiStrumento.addActionListener(new btnAddInstrumentListener(this));
+
 		this.setVisible(true);
 	}
-	
-	
-	
+
+
+
 
 	public void addInstrument()
 	{
 		//value prende il valore che è attualmente selezionato nella ComboBox
 		String value = cbStrumenti.getSelectedItem().toString();
-		
+
 		if(dataValidator.checkString(value) && !listModel3.contains(value))
 		{ 
 			listModel3.addElement(value);
 		}
 	}
-	
+
 	public void removeInstrument()
 	{
 		if(list.getSelectedValue() != null)
@@ -147,24 +147,24 @@ public class aggiungiStrumentoWnd extends JFrame{
 			listModel3.remove(list.getSelectedIndex());
 		}
 	}
-	
+
 
 	public void close()
 	{
 		caller.setEnabled(true);
 		caller.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		ArrayList<String> instrumentList=new ArrayList();
-		
+
 		for(int i=0; i<listModel3.size(); i++)
 		{
 			instrumentList.add(listModel3.getElementAt(i));
 		}
 		((areaRiservataWnd) caller).setInstrumentList(instrumentList);
 	}
-	
+
 	private void loadModel()
 	{		
 		listModel3=((areaRiservataWnd)caller).getInstrumentList();
 	}
-	
+
 }
